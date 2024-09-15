961d5dccbfb5172754c127ce7c570c762381bd1d
luizalec7 <luiz.alecsander@gmail.com>
2024-09-15 17:10:46 -0300

    Primeiro projeto Spring funcional.

---
+++ src/main/java/academy/devdojo/Springboot2_essentials/controller/AnimeController.java
@@ -0,0 +1,18 @@
+package academy.devdojo.Springboot2_essentials.controller;
+
+import academy.devdojo.Springboot2_essentials.domain.Anime;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("anime")
+public class AnimeController {
+    @GetMapping(path = "list")
+    public List<Anime> list(){
+        return List.of(new Anime("DBZ"), new Anime("Berserk"));
+    }
+}
--- src/main/java/academy/devdojo/Springboot2_essentials/start/Springboot2EssentialsApplication.java
+++ src/main/java/academy/devdojo/Springboot2_essentials/start/Springboot2EssentialsApplication.java
@@ -1,9 +1,11 @@
-package academy.devdojo.Springboot2_essentials;
+package academy.devdojo.Springboot2_essentials.start;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "academy.devdojo.Springboot2_essentials")
 public class Springboot2EssentialsApplication {
 
 	public static void main(String[] args) {
