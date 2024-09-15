bf1b4f07652d5107be527c4336038b49cf0987c9
luizalec7 <luiz.alecsander@gmail.com>
2024-09-15 13:48:36 -0300

    Commit 1: montando projeto

---
+++ src/main/java/academy/devdojo/Springboot2_essentials/Springboot2EssentialsApplication.java
@@ -0,0 +1,13 @@
+package academy.devdojo.Springboot2_essentials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2EssentialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2EssentialsApplication.class, args);
+	}
+
+}
