6a5fc613e2e4bbd5a1ca44f397a3f799839deeb4
luizalec7 <luiz.alecsander@gmail.com>
2024-09-17 15:37:31 -0300

    Metodo GET - Parte 2

--- src/main/java/academy/devdojo/Springboot2_essentials/service/AnimeService.java
+++ src/main/java/academy/devdojo/Springboot2_essentials/service/AnimeService.java
@@ -2,13 +2,26 @@ package academy.devdojo.Springboot2_essentials.service;
 
 import academy.devdojo.Springboot2_essentials.domain.Anime;
 import academy.devdojo.Springboot2_essentials.repository.AnimeRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 public class AnimeService {
+
+    private List<Anime> animes = List.of(new Anime(7L,"Cristiano Ronaldo"), new Anime(10L,"Messi"));
+
     public List<Anime> listAll() {
-        return  List.of(new Anime(7L,"Cristiano Ronaldo"), new Anime(10L,"Messi"));
+        return animes;
+    }
+
+    public Anime findById(long id) {
+        return animes.stream()
+                .filter(anime -> anime.getId() == id)
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Jogador NOT FOUND!"));
+
     }
 }
